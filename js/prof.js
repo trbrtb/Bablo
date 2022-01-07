050862942cf6f8a24d60e4865f675234763c66b0
trbrtb <76260832+trbrtb@users.noreply.github.com>
2022-01-07 14:34:05 +0400

    1

--- js/prof.js
+++ js/prof.js
@@ -1,4 +1,4 @@
 type="text/javascript">  
-$.get('https://www.gonibablo.ml//otkuda.html', function (data) {  
+$.get('https://www.gonibablo.ml/otkuda.html', function (data) {  
   $('#loadcontent').html( $('.ameaning', data).html() )
 });
